3f5f6917618ae30c3bbafc4de5b4ac52a7d6cc0e
oladeji21 <oladejiajayi@gmail.com>
2023-06-15 15:51:12 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,8 @@
  *  main - A program that print a line with function
  *   Reture: 0 (successful)
  */
+int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+puts("\ "Programming is like building a multilingual puzzle");
+return (0):
 }
