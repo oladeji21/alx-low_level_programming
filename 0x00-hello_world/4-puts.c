71d28d843c5f4a9202f322a1b6217a5f4014e055
oladeji21 <oladejiajayi@gmail.com>
2023-06-15 14:50:32 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ *  main - A program that print a line with function
+ *   Reture: 0 (successful)
+ */
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
