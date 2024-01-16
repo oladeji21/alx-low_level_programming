74fede45730c38de8f69097ac8952421310cadd6
oladeji21 <oladejiajayi05@gmail.com>
2024-01-15 16:55:35 -0800

    3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-wget -p 
+wget -p.. https://raw.githubusercontent.com/oladeji21/alx-low_level_programming/master/0x18-dynamic_libraries/libhack.so 
+export LD_PRELOAD="$PWD/../libhack.so"
